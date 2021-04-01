ed43ffa1c882eaa22fa849dc46c28edbd703b4f8
Marat <marathovsepyan45@gmail.com>
2021-04-01 10:53:15 +0400

    testing page

---
+++ precache-manifest.ea0ad9d90368838192ca6be424b1182d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5627f8ebd5a00398d2a5",
+    "url": "/css/app.877c9838.css"
+  },
+  {
+    "revision": "5396e8056e729e189ba6",
+    "url": "/css/chunk-284189c8.ff4a0cb0.css"
+  },
+  {
+    "revision": "3baa9dc4a087fabe39a8",
+    "url": "/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "671322187375a0e7eeb5fa7dd0c6796c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5627f8ebd5a00398d2a5",
+    "url": "/js/app.8b6f92c1.js"
+  },
+  {
+    "revision": "5396e8056e729e189ba6",
+    "url": "/js/chunk-284189c8.a5c9069b.js"
+  },
+  {
+    "revision": "3baa9dc4a087fabe39a8",
+    "url": "/js/chunk-vendors.9e7cc681.js"
+  },
+  {
+    "revision": "0e28c2433bf8b685ad4c77e76ec45621",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
